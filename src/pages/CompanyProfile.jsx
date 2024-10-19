4ebde35aeb2831fb263db4a155575965c3a2dbd1
Musa <musaharoon.2003@gmail.com>
2024-10-19 17:02:09 +0500

    integrated meeting minutes, money, fixed bugs on tenant profile

--- src/pages/CompanyProfile.jsx
+++ src/pages/CompanyProfile.jsx
@@ -354,14 +354,14 @@ const Company = ({ role }) => {
                 nonNustian: fetchedData.nonNustianInterns,
                 total: fetchedData.nustianInterns + fetchedData.nonNustianInterns,
               },
-              gatePasses: fetchedData.gatepasses,
               workPermits: fetchedData.workpermits,
               violations: fetchedData.violations,
               contractDuration: contractDurationPercentage,
               employees: fetchedData.employees,
               meetingMinutes: fetchedData.meetingMinutes,
-              eTags: { "Received": 20, "Pending": 10 }, //change this after api call
-              gatePasses: { "Received": 20, "Pending": 10 },
+              meetingMinutesMoney: fetchedData.meetingMinutesMoney,
+              eTags: fetchedData.etags,
+              gatePasses: fetchedData.gatepasses,
             };
             console.log("setting company data to: ", consolidatedData)
             setCompanyData(consolidatedData);
@@ -420,14 +420,14 @@ const Company = ({ role }) => {
                 nonNustian: fetchedData.nonNustianInterns,
                 total: fetchedData.nustianInterns + fetchedData.nonNustianInterns,
               },
-              gatePasses: fetchedData.gatepasses,
               workPermits: fetchedData.workpermits,
               violations: fetchedData.violations,
               contractDuration: contractDurationPercentage,
               employees: fetchedData.employees,
               meetingMinutes: fetchedData.meetingMinutes,
-              eTags: { "Received": 20, "Pending": 10 }, //after api call change this to fetchedData.etags and the one below it too
-              gatePasses: { "Received": 20, "Pending": 10 }
+              meetingMinutesMoney: fetchedData.meetingMinutesMoney,
+              eTags: fetchedData.etags,
+              gatePasses: fetchedData.gatepasses,
             };
             console.log("setting company data to: ", consolidatedData)
             setCompanyData(consolidatedData);
